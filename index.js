52eda3bf719b17bda5b8a0432fb3f34932a8f589
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-08-07 09:33:18 +0200

    create post route

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 // dotenv loads parameters (port and database config) from .env
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const connection = require('./db');
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const connection = require('./db')
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-// respond to requests on `/api/users`
 app.get('/api/users', (req, res) => {
-  // send an SQL query to get all users
   connection.query('SELECT * FROM user', (err, results) => {
     if (err) {
-      // If an error has occurred, then the client is informed of the error
       res.status(500).json({
         error: err.message,
         sql: err.sql,
       });
     } else {
-      // If everything went well, we send the result of the SQL query as JSON
-      res.json(results);
+      res.json(results)
     }
-  });
-});
+  })
+})
+
+app.post('/api/users', (req, res) => {
+  const data = req.body
+
+  connection.query('INSERT INTO user SET ?', data, (err, results) => {
+    if (err) {
+      res.status(500).json({
+        error: err.message,
+        sql: err.sql,
+      });
+    } else {
+      res.json(results)
+    }
+  })
+})
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
-    throw new Error('Something bad happened...');
+    throw new Error('Something bad happened...')
   }
 
-  console.log(`Server is listening on ${process.env.PORT}`);
-});
+  console.log(`Server is listening on ${process.env.PORT}`)
+})
